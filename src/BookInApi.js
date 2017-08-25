043e7e8dd99bf00f2fe9a3bd2144c76156e4e0ec
Wilfried Reiter <wilfried.reiter@devpoint.at>
2017-08-25 15:16:13 +0200

    added constructor parameter for CacheMap

--- src/BookInApi.js
+++ src/BookInApi.js
@@ -8,17 +8,17 @@ class BookInApi extends Api {
 
     _createCaches() {
         return {
-            'accommodation': new CacheMap(),
-            'booking': new CacheMap(),
-            'hotel': new CacheMap(),
-            'invoice': new CacheMap(),
-            'offer': new CacheMap(),
-            'payment': new CacheMap(),
-            'room': new CacheMap(),
-            'roomprice': new CacheMap(),
-            'season': new CacheMap(),
-            'term': new CacheMap(),
-            'user': new CacheMap(),
+            'accommodation': new CacheMap('accommodation'),
+            'booking': new CacheMap('booking'),
+            'hotel': new CacheMap('hotel'),
+            'invoice': new CacheMap('invoice'),
+            'offer': new CacheMap('offer'),
+            'payment': new CacheMap('payment'),
+            'room': new CacheMap('room'),
+            'roomprice': new CacheMap('roomprice'),
+            'season': new CacheMap('season'),
+            'term': new CacheMap('term'),
+            'user': new CacheMap('user'),
         };
     }
 
