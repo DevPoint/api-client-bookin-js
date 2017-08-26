93448d52d9a73e872975504851afd5abbfad1ecc
Wilfried Reiter <wilfried.reiter@devpoint.at>
2017-08-26 08:27:35 +0200

    added parameter entriesAreObservables

--- src/BookInApi.js
+++ src/BookInApi.js
@@ -8,17 +8,17 @@ class BookInApi extends Api {
 
     _createCaches() {
         return {
-            'accommodation': new CacheMap('accommodation'),
-            'booking': new CacheMap('booking'),
-            'hotel': new CacheMap('hotel'),
-            'invoice': new CacheMap('invoice'),
-            'offer': new CacheMap('offer'),
-            'payment': new CacheMap('payment'),
-            'room': new CacheMap('room'),
-            'roomprice': new CacheMap('roomprice'),
-            'season': new CacheMap('season'),
-            'term': new CacheMap('term'),
-            'user': new CacheMap('user'),
+            'accommodation': new CacheMap('accommodation', true),
+            'booking': new CacheMap('booking', true),
+            'hotel': new CacheMap('hotel', true),
+            'invoice': new CacheMap('invoice', true),
+            'offer': new CacheMap('offer', true),
+            'payment': new CacheMap('payment', true),
+            'room': new CacheMap('room', true),
+            'roomprice': new CacheMap('roomprice', true),
+            'season': new CacheMap('season', true),
+            'term': new CacheMap('term', true),
+            'user': new CacheMap('user', true),
         };
     }
 
