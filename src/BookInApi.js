dd0bc3a2ccb41e11527cce7c14366dfbd5477910
Wilfried Reiter <wilfried.reiter@devpoint.at>
2017-08-27 06:18:21 +0200

    added entrySchema as CacheMap constructor parameter

--- src/BookInApi.js
+++ src/BookInApi.js
@@ -8,17 +8,17 @@ class BookInApi extends Api {
 
     _createCaches() {
         return {
-            'accommodation': new CacheMap('accommodation', true),
-            'booking': new CacheMap('booking', true),
-            'hotel': new CacheMap('hotel', true),
-            'invoice': new CacheMap('invoice', true),
-            'offer': new CacheMap('offer', true),
-            'payment': new CacheMap('payment', true),
-            'room': new CacheMap('room', true),
-            'roomprice': new CacheMap('roomprice', true),
-            'season': new CacheMap('season', true),
-            'term': new CacheMap('term', true),
-            'user': new CacheMap('user', true),
+            'accommodation': new CacheMap('accommodation', {}, true),
+            'booking': new CacheMap('booking', {}, true),
+            'hotel': new CacheMap('hotel', {}, true),
+            'invoice': new CacheMap('invoice', {}, true),
+            'offer': new CacheMap('offer', {}, true),
+            'payment': new CacheMap('payment', {}, true),
+            'room': new CacheMap('room', {}, true),
+            'roomprice': new CacheMap('roomprice', {}, true),
+            'season': new CacheMap('season', {}, true),
+            'term': new CacheMap('term', {}, true),
+            'user': new CacheMap('user', {}, true),
         };
     }
 
