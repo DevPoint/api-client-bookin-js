e1bedccf60cf4884a2e09b138bab6664c13cf69c
Wilfried Reiter <wilfried.reiter@devpoint.at>
2017-08-27 08:48:12 +0200

    all types

--- src/cache/types.js
+++ src/cache/types.js
@@ -17,6 +17,52 @@ export default {
         updated_at: PropTypes.string
     },
 
+    'room': {
+        id: PropTypes.nullableInteger,
+        hotel_id: PropTypes.nullableInteger,
+        slug: PropTypes.string,
+        name: PropTypes.string,
+        description: PropTypes.string,
+        hidden: PropTypes.boolean,
+        locked: PropTypes.boolean
+        created_at: PropTypes.string,
+        updated_at: PropTypes.string
+    },
+
+    'roomprice': {
+        id: PropTypes.nullableInteger,
+        hotel_id: PropTypes.nullableInteger,
+        created_at: PropTypes.string,
+        updated_at: PropTypes.string
+    },
+
+    'season': {
+        id: PropTypes.nullableInteger,
+        hotel_id: PropTypes.nullableInteger,
+        slug: PropTypes.string,
+        name: PropTypes.string,
+        description: PropTypes.string,
+        hidden: PropTypes.boolean,
+        locked: PropTypes.boolean,
+        periods: PropTypes.array
+        created_at: PropTypes.string,
+        updated_at: PropTypes.string
+    },
+
+    'term': {
+        id: PropTypes.nullableInteger,
+        hotel_id: PropTypes.nullableInteger,
+        parent_id: PropTypes.nullableInteger,
+        taxonomy: PropTypes.string,
+        slug: PropTypes.string,
+        name: PropTypes.string,
+        description: PropTypes.string,
+        hidden: PropTypes.boolean,
+        locked: PropTypes.boolean
+        created_at: PropTypes.string,
+        updated_at: PropTypes.string
+    },
+
     'booking': {
         id: PropTypes.nullableInteger,
         hotel_id: PropTypes.nullableInteger,
@@ -57,62 +103,41 @@ export default {
         updated_at: PropTypes.string
     },
 
-    'room': {
-        id: PropTypes.nullableInteger,
-        hotel_id: PropTypes.nullableInteger,
-        slug: PropTypes.string,
-        name: PropTypes.string,
-        description: PropTypes.string,
-        hidden: PropTypes.boolean,
-        locked: PropTypes.boolean
-        created_at: PropTypes.string,
-        updated_at: PropTypes.string
-    },
-
-    'roomprice': {
-
-    },
-
-    'season': {
-        id: PropTypes.nullableInteger,
-        hotel_id: PropTypes.nullableInteger,
-        slug: PropTypes.string,
-        name: PropTypes.string,
-        description: PropTypes.string,
-        hidden: PropTypes.boolean,
-        locked: PropTypes.boolean,
-        periods: PropTypes.array
-        created_at: PropTypes.string,
-        updated_at: PropTypes.string
-    },
-
-    'term': {
-        id: PropTypes.nullableInteger,
-        hotel_id: PropTypes.nullableInteger,
-        parent_id: PropTypes.nullableInteger,
-        taxonomy: PropTypes.string,
-        slug: PropTypes.string,
-        name: PropTypes.string,
-        description: PropTypes.string,
-        hidden: PropTypes.boolean,
-        locked: PropTypes.boolean
-        created_at: PropTypes.string,
-        updated_at: PropTypes.string
-    },
-
     'enquiry': {
-
+        id: PropTypes.nullableInteger,
+        hotel_id: PropTypes.nullableInteger,
+        title: PropTypes.string,
+        firstName: PropTypes.string,
+        lastName: PropTypes.string,
+        extraName: PropTypes.string,
+        street: PropTypes.string,
+        city: PropTypes.string,
+        zip: PropTypes.string,
+        country: PropTypes.string,
+        email: PropTypes.string,
+        phone: PropTypes.string,
+        created_at: PropTypes.string,
+        updated_at: PropTypes.string
     },
 
     'invoice': {
-
+        id: PropTypes.nullableInteger,
+        hotel_id: PropTypes.nullableInteger,
+        created_at: PropTypes.string,
+        updated_at: PropTypes.string
     },
 
     'offer': {
-
+        id: PropTypes.nullableInteger,
+        hotel_id: PropTypes.nullableInteger,
+        created_at: PropTypes.string,
+        updated_at: PropTypes.string
     },
 
     'payment': {
-
+        id: PropTypes.nullableInteger,
+        hotel_id: PropTypes.nullableInteger,
+        created_at: PropTypes.string,
+        updated_at: PropTypes.string
     }
 };
