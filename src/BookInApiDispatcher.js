515c2b0268bbf529e6bc2e7df21c5c0ee1b365e4
Wilfried Reiter <wilfried.reiter@devpoint.at>
2017-08-28 11:23:34 +0200

    overload dispatch function

--- src/BookInApiDispatcher.js
+++ src/BookInApiDispatcher.js
@@ -3,6 +3,29 @@ import { ApiDispatcher } from 'api-client-core';
 
 class BookInApiDispatcher extends ApiDispatcher {
 
+    dispatch(action) {
+        const actionTypeFrags = action.type.split('_');
+        if (actionTypeFrags.length >= 2) {
+            if (actionTypeFrags[1] === 'TRANSACTION') {
+                let transactionType = actionTypeFrags[2].toLowerCase();
+                if (actionTypeFrags.length >= 5 && actionTypeFrags[3] === 'BY' && actionTypeFrags[4] === 'SLUG') {
+                	transactionType += 'BySlug';
+                }
+                const transactionDispatcher = this._getTransactionDispatcher(transactionType);
+                transactionDispatcher.dispatch(this._api.transactions(), action);
+            }
+            else if (actionTypeFrags[1] === 'VIEW') {
+                const viewDispatcher = this._getViewDispatcher();
+                viewDispatcher.dispatch(this._api.views(), action);
+            }
+            else if (actionTypeFrags[1] === 'CACHE') {
+                const itemType = actionTypeFrags[2].toLowerCase();
+                const cacheDispatcher = this._getCacheDispatcher(itemType);
+                cacheDispatcher.dispatch(this._api.cache(itemType), action);
+            }
+        }
+        return action;
+    }
 }
 
 export default BookInApiDispatcher;
