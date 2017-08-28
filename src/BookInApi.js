c60b934debb99642401bd93d8ebbddd171d63a88
Wilfried Reiter <office@devpoint.at>
2017-08-28 12:59:23 +0200

    added load..View functions and hotelInsertTransaction and hotelUpdateTransaction functions

--- src/BookInApi.js
+++ src/BookInApi.js
@@ -49,6 +49,45 @@ class BookInApi extends Api {
         return transactionActions.updateBySlugStart(this.nameSpace, transactionId, itemType, slug, data);
     }
 
+    hotelInsertTransaction(transactionId, itemType, hotelid, data) {
+        return this._client.hotelInsertTransaction(transactionId, itemType, hotelid, data);
+    }
+
+    hotelUpdateTransactionBySlug(transactionId, itemType, hotelid, slug, data) {
+        return this._client.hotelUpdateTransactionBySlug(transactionId, itemType, hotelid, slug, data);
+    }
+
+    loadHotelView(viewId, hotelId, builder) {
+        return this._client.loadHotelView(viewId, hotelId, builder);
+    }
+
+    loadHotelViewBySlug(viewId, itemType, hotelId, slug, eagerType) {
+        return this._client.loadHotelViewBySlug(viewId, itemType, hotelId, slug, eagerType);
+    }
+
+    loadBookingView(viewId, bookingId, builder) {
+        return this._client.loadBookingView(viewId, bookingId, builder);
+    }
+
+    loadCustomerView(viewId, customerId, builder) {
+        return this._client.loadCustomerView(viewId, customerId, builder);
+    }
+
+    hotelTaxonomyTermInsertTransaction(transactionId, hotelid, data) {
+        return this._client.hotelTaxonomyTermInsertTransaction(transactionId, hotelid, data);
+    }
+
+    hotelTaxonomyTermUpdateTransactionBySlug(transactionId, hotelid, taxonomy, slug, data) {
+        return this._client.hotelTaxonomyTermUpdateTransactionBySlug(transactionId, hotelid, taxonomy, slug, data);
+    }
+
+    loadHotelTaxonomyTermView(viewId, hotelId, taxonomy, builder) {
+        return this._client.loadHotelTaxonomyTermView(viewId, hotelId, taxonomy, builder);
+    }
+
+    loadHotelTaxonomyTermViewBySlug(viewId, hotelId, taxonomy, slug, eagerType) {
+        return this._client.loadHotelTaxonomyTermViewBySlug(viewId, hotelId, taxonomy, slug, eagerType);
+    }    
 }
 
 export default BookInApi;
